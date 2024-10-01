62ac3b0abdb0fc74296953260795356dc56e7aec
dnyaneshkamthe <dnyanesh.kamthe@avestantechnologies.in>
2024-10-01 14:08:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors')
+
+app.use(cors());
+app.use(express.json());
+
+//data
+const users = [
+    {
+      id: 1,
+      username: 'octocat',
+      name: 'The Octocat',
+      repoCount: 8,
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      username: 'torvalds',
+      name: 'Linus Torvalds',
+      repoCount: 25,
+      location: 'Portland',
+    },
+    {
+      id: 3,
+      username: 'gaearon',
+      name: 'Dan Abramov',
+      repoCount: 50,
+      location: 'London',
+    },
+    {
+      id: 4,
+      username: 'addyosmani',
+      name: 'Addy Osmani',
+      repoCount: 42,
+      location: 'Mountain View',
+    },
+    {
+      id: 5,
+      username: 'tj',
+      name: 'TJ Holowaychuk',
+      repoCount: 150,
+      location: 'Victoria',
+    },
+  ];
+
+
+//endpoints
+app.get("/users", (req, res) => {
+    res.json(users);
+})
+
+app.get("/users/:id", (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if(!user) return res.status(404).send("User not found");
+    res.json(user);
+})
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
